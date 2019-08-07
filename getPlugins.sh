ebd9580b05c86a266db04298da66a4e58ad82e75
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2019-08-07 14:45:46 -0400

    Added normalization step

--- getPlugins.sh
+++ getPlugins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+git clone https://github.com/movingpictures83/CSVNormalize.git ../../plugins/CSVNormalize
 git clone https://github.com/movingpictures83/Threshold.git ../../plugins/Threshold
 git clone https://github.com/movingpictures83/CSVExtract.git ../../plugins/CSVExtract
